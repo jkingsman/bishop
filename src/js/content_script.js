8570ed7f34538ea5902710c93c6a0d2dbb7a7a90
Jack Kingsman <jack.kingsman@gmail.com>
2015-02-06 23:43:26 -0800

    Tweaked blocking

--- src/js/content_script.js
+++ src/js/content_script.js
@@ -20,11 +20,12 @@ function doScan(recursive) {
     var currentScanUrl = stripTrailingSlash(window.location.href);
 
     if (config.exclusionList.length > 0) {
-  //we have an exclusion list to work with; break it out
+	//we have an exclusion list to work with; break it out
 	var excludes = config.exclusionList.split("::");
+	console.log(excludes);
 	for (var i = 0; i < excludes.length; i++) {
-	    if(currentScanUrl.indexOf(excludes[i]) > -1){
-	  //this page contains a blocked url string; get outta here
+	    if(currentScanUrl.indexOf(excludes[i]) > -1 && excludes[i].length > 0){
+		//this page contains a blocked url string and it's not an empty string; get outta here
 		return;
 	    }
 	}
