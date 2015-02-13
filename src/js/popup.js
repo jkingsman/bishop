a3863f0f3b8fb286daf5193de0c22bcc5b3bf06d
Jack Kingsman <jack.kingsman@gmail.com>
2015-02-13 00:38:36 -0800

    popup shows dis/enabled OR queuing

--- src/js/popup.js
+++ src/js/popup.js
@@ -7,32 +7,22 @@ var syncRules;
 
 function init_main() {
     //get the current enabled state and rule list
-    chrome.storage.sync.get('status', function (data) {
-        if (typeof data.status === "undefined") {
-            //this is first use; enable by default and save
-            chrome.storage.sync.set({
-                "status": 1
-            });
-            var isEnabled = 1;
-        }
-        else {
-            var isEnabled = data.status;
-        }
-
+    chrome.storage.sync.get(null, function (data) {
         //make the switch reflect our current state
-        if (isEnabled) {
+        if (data.status) {
             $("#status").html('<span class="bg-success">Enabled</span>');
-        }
-        else {
+        } else if(data.config.enableQueue) {
+            $("#status").html('<span class="bg-warning">Queueing</span>');
+        } else{
             $("#status").html('<span class="bg-danger">Disabled</span>');
         }
+        
+        //build options link
+        $("#optLink").attr("href", chrome.extension.getURL("html/options.html"));
+    
+        //show the menu
+        $('html').hide().fadeIn('slow');
     });
-
-    //build options link
-    $("#optLink").attr("href", chrome.extension.getURL("html/options.html"));
-
-    //show the menu
-    $('html').hide().fadeIn('slow');
 }
 
 //bind events to dom elements
