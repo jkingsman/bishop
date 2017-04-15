aae9d902450f2a67d898fd511b3fb34da5404dff
Jack Kingsman <jack.kingsman@gmail.com>
2017-04-14 21:08:35 -0700

    Fix labelling of rule in popup

--- src/js/content_script/content_script.js
+++ src/js/content_script/content_script.js
@@ -93,7 +93,7 @@ function addSiteAndAlert(url, rule) {
             //the timeout is here due to some weird issue where, without a timeout, alert dismissal is required before the audio plays
             //I'm guessing it's some issue with async processes getting blocked but who knows. this seems to fix it.
             setTimeout(function () {
-                alert('&#9821; Bishop matched your rule ' + rule + ' at ' + url);
+                alert('&#9821; Bishop matched your rule "' + rule + '" at ' + url);
             }, 500);
         }
 
